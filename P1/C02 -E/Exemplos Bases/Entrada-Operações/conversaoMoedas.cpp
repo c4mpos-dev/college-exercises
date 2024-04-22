9f7a25f61c529336eeaa206287e6c28b888836ec
Cauã Campos <caua.campos310506@gmail.com>
2024-04-22 20:33:22 -0300

    Classes before the first exam

--- P1/C02 -E/Exemplos Bases/Entrada-Operações/conversaoMoedas.cpp
+++ P1/C02 -E/Exemplos Bases/Entrada-Operações/conversaoMoedas.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main()
 {
-	//Exemplos Iniciais com conversão de moedas
+	//Exemplos Iniciais com conversão de moedas.
 
     double real, dolar, euro;
 
     cout << "Digite o valor em reais: " << endl;
     cin >> real;
 
-    //Converte o valor para dólar e euro
+    //Converte o valor para dólar e euro.
     dolar = real / 4.95;
     euro = real / 5.4;
 
-    //Imprime o valor em dólar e euro
+    //Imprime o valor em dólar e euro.
 
     cout << "\nU$ " << dolar << endl;
     cout << "E " << fixed << setprecision(2) << euro;
