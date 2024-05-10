1bcba0dc83f308897c98b6eaa3e5db278d58030f
c4mpos-dev <campos.eet@gmail.com>
2024-05-09 22:06:03 -0300

    Resolution of the "Whindersson Nunes" exercise

--- P1/C02 -E/Exercícios Moodle/Vetores/WhinderssonNunes.cpp
+++ P1/C02 -E/Exercícios Moodle/Vetores/WhinderssonNunes.cpp
@@ -5,6 +5,26 @@ using namespace std;
 
 int main(){
 
+    int nVideos = 0, nMaior = 0, nMenor = 0;
     
-    
-}
+    do {
+        cin >> nVideos;
+    }while(nVideos < 0 || nVideos > 1000);
+
+    vector<int> nViews(nVideos);
+
+    for (int i = 0; i < nVideos; i++)
+        cin >> nViews[i];
+
+    for (int i = 0; i < nVideos; i++){
+        if (nViews[i] > 10000000)
+            nMaior++;
+        else
+            nMenor++;
+
+    }
+        
+    cout << nMaior << " video(s) com mais de 10M views" << endl;
+    cout << nMenor << " video(s) com menos de 10M views" << endl;
+
+}   
