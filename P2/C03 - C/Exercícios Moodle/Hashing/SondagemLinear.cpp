d578c823fe4d50008d2bcc183816af6ef7c038e4
c4mpos-dev <campos.eet@gmail.com>
2024-10-01 22:49:27 -0300

    Resolution of the "SondagemLinear" exercise

---
+++ P2/C03 - C/Exercícios Moodle/Hashing/SondagemLinear.cpp
@@ -0,0 +1,28 @@
+#include<iostream> 
+using namespace std;
+
+int hash_aux(int k, int m){
+    int result = k % m;
+
+    if (result < 0)
+        result += m;
+    
+    return result;
+}
+
+int hash1(int k, int i, int m) {
+    return (hash_aux(k, m) + i) % m;
+}
+
+int main() {
+    int result, k, m;
+
+    cin >> k >> m;
+
+    for (int i = 0; i < m; ++i) {
+        int position = hash1(k, i, m);
+        cout << position << " ";
+    }
+
+    return 0;
+}
