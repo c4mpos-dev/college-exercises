dff6580568714f0c55e3baa44c56317a6a5239c2
Cauã Campos <campos.eet@gmail.com>
2025-04-02 00:46:56 -0300

    :sparkles: Optimizing the "aConquistaDeNós" exercise

--- P3/C04 - B/Exercícios Moodle/Grafos/aConquistaDeNós.cpp
+++ P3/C04 - B/Exercícios Moodle/Grafos/aConquistaDeNós.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
-
 using namespace std;
+
+#define MAX 100
 
 int main() {
     int N, M;
     cin >> N >> M;
-
-    int matriz[100][100] = {0};
-
+    
+    int matriz[MAX][MAX] = {0};
+    
     for (int i = 0; i < M; i++) {
         int A, B;
         cin >> A >> B;
         matriz[A - 1][B - 1] = 1;
         matriz[B - 1][A - 1] = 1;
     }
-
+    
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             cout << matriz[i][j] << " ";
         }
         cout << endl;
     }
-
+    
     return 0;
 }
