4d6d80773731b439f2343bd5943f861223e6d46e
Cauã Campos <campos.eet@gmail.com>
2026-02-20 22:15:07 -0300

    feat: exercise 2 - report 01- e09

---
+++ P5/E09/Relatório 01/exercicio_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    scanf("%d", &n);
+
+    if (n < 0 || n > 50) {
+        printf("Valor invalido\n");
+        return 0;
+    }
+
+    for (i = n - 1; i >= 1; i--) {
+        printf("Resto da divisao de %d por %d: %d\n", n, i, n % i);
+    }
+
+    return 0;
+}
