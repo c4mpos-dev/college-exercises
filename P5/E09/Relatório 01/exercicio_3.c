347a77fb35536aada7da27367eecff85430611ee
Cauã Campos <campos.eet@gmail.com>
2026-02-20 22:24:39 -0300

    feat: exercise 3 - report 01- e09

---
+++ P5/E09/Relatório 01/exercicio_3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void analisarNumeros(int a, int b, int c, int d, int e) {
+    int pares = 0, impares = 0, positivos = 0, negativos = 0;
+    int v[5] = {a, b, c, d, e};
+
+    for (int i = 0; i < 5; i++) {
+        if (v[i] % 2 == 0)
+            pares++;
+        else
+            impares++;
+
+        if (v[i] >= 0)
+            positivos++;
+        else if (v[i] < 0)
+            negativos++;
+    }
+
+    printf("Quantidade de numeros pares: %d\n", pares);
+    printf("Quantidade de numeros impares: %d\n", impares);
+    printf("Quantidade de numeros positivos: %d\n", positivos);
+    printf("Quantidade de numeros negativos: %d\n", negativos);
+}
+
+int main() {
+    int n;
+    int a, b, c, d, e;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+        analisarNumeros(a, b, c, d, e);
+    }
+
+    return 0;
+}
