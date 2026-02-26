ad73b7db389feca04c4d2dd6010f20cd875327cd
c4mpos-dev <campos.eet@gmail.com>
2026-02-25 22:42:47 -0300

    fix: exercise "ZéDoLanche" fixed

--- P5/C06/Aula-03/ZéDoLanche.java
+++ P5/C06/Aula-03/ZéDoLanche.java
@@ -8,9 +8,9 @@ public class Main {
         int terceiraHora = 2;
 
         int soma = primeiraHora + segundaHora + terceiraHora;
-        double  media = soma / horas;
+        double  media = (double) soma / horas;
 
-        System.out.println("Total: ", + soma);
-        System.out.println("Média: ", + media);
+        System.out.println("Total: " + soma);
+        System.out.println("Média: " + media);
     }
 }
